2a812b4e50b7571fbb995b83ef2e79393188523c
patatahooligan <filwn.oikonomou@gmail.com>
2020-09-20 14:39:50 +0300

    Enable moving the scope guard

--- scope-guard-test.cpp
+++ scope-guard-test.cpp
@@ -2,11 +2,21 @@
 
 #include <iostream>
 
+auto move_scope_guard() {
+    OnScopeExit on_scope_exit = []() {
+        std::cout <<
+            "This OnScopeExit was moved and should trigger when main exits\n";
+    };
+    return on_scope_exit;
+}
+
 int main()
 {
-    OnScopeExit on_scope_exit = []() {
-        std::cout << "Exiting scope\n";
+    const OnScopeExit on_scope_exit = []() {
+        std::cout << "This should trigger last when main exits\n";
     };
 
-    std::cout << "This should print first\n";
+    const auto moved_scope_exit = move_scope_guard();
+
+    std::cout << "Main is about to exit\n";
 }
--- scope-guard.hpp
+++ scope-guard.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <concepts>
+#include <utility>
 
 template<std::invocable CallableType>
 class OnScopeExit {
@@ -11,7 +12,19 @@ class OnScopeExit {
         }
 
         OnScopeExit(CallableType exit_function):
-            exit_function(exit_function) {}
+            exit_function(std::move(exit_function)) {}
+
+        OnScopeExit(OnScopeExit &&other)
+        {
+            exit_function = std::move(other.exit_function);
+            other.active = false;
+        }
+
+        // Defining the above constructors should prevent the implicit
+        // declaration of these, but I let's show intent by explicitly
+        // forbidding them
+        OnScopeExit() = delete;
+        OnScopeExit(const OnScopeExit&) = delete;
 
         ~OnScopeExit()
         {
